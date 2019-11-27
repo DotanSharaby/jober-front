b6475829e10b5700d50ffdf3d8adef98d5ee9312
DotanSharaby <dotan.sharaby@gmail.com>
2019-11-27 18:03:31 +0200

    commit for pull

---
+++ src/views/Job-Edit.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <h1 v-if="job.id">Edit Job</h1>
+    <h1 v-else>Add Job</h1>
+  </section>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      job: {}
+    };
+  },
+  async created() {
+    const jobId = this.$route.params.id;
+    if (this.jobId) {
+      this.job = await this.$store.dispatch({ type: "getJob", id: jobId });
+    }
+  }
+};
+</script>
