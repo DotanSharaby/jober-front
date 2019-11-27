b835078ef34fb7877e071ea6b78a3958721974e4
kostagon <49278316+kostagon@users.noreply.github.com>
2019-11-27 18:54:46 +0200

    commit jobDetails (nothing interesting, just pulling master)

---
+++ src/views/JobDetails.vue
@@ -0,0 +1,35 @@
+<template>
+  <section v-if="job" class="job-details">
+    <section class="flex">
+      <div class="avatar">
+        <img :src="logoUrl" alt />
+      </div>
+    </section>
+    <pre>{{job}}</pre>
+  </section>
+</template>
+
+<script>
+import JobService from "@/services/JobService";
+
+export default {
+  data() {
+    return {
+      job: null
+    };
+  },
+  computed: {
+    logoUrl() {
+      return this.job.owner.logoUrl;
+    }
+  },
+  async created() {
+    const id = this.$route.params.id;
+    const job = await JobService.getById(id);
+    this.job = job;
+  }
+};
+</script>
+
+<style>
+</style>
