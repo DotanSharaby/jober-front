df2f2dbd85f7343c7661cdd8169c63a46586a626
DotanSharaby <dotan.sharaby@gmail.com>
2019-11-28 16:32:50 +0200

    clogs and debug clean

--- src/services/UtilService.js
+++ src/services/UtilService.js
@@ -14,15 +14,12 @@ function makeId(length = 5) {
 }
 
 function upload(ev) {
-    // debugger;
-
     const CLOUD_NAME = 'dvbqwqxqs'
     const PRESET_NAME = 'ockqw801'
     const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/upload`
 
     const formData = new FormData();
-    if (!ev.target) formData.append('file', ev)
-    else formData.append('file', ev.target.files[0])
+    formData.append('file', ev.target.files[0]);
     formData.append('upload_preset', PRESET_NAME);
 
     return fetch(UPLOAD_URL, {
@@ -31,7 +28,7 @@ function upload(ev) {
     })
         .then(res => res.json())
         .then(res => {
-            console.log(res)
+            // console.log(res)
             return res
         })
         .catch(err => console.error(err))
