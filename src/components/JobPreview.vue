60f7f2495b09835f67feb0fd86f4596a80722471
Meital Lazarovich <lazarovich.meital@gmail.com>
2019-11-28 13:34:49 +0200

    better flex layout

--- src/components/JobPreview.vue
+++ src/components/JobPreview.vue
@@ -2,22 +2,24 @@
     <section @click="goToDetails" class="job-preview flex column space-between align-center">
         <h3 v-if="msg">{{msg}}</h3>
         <div class="icons flex space-between align-center text-center">
-          <h2>❤</h2>
-          <h2>🗑</h2>
+            <h2 class="trash">🗑</h2>
+            <h2>❤</h2>
         </div>
         <img class="job-img" :src="job.img" />
-        <div class="details flex space-between align-center">
-            <div class="main flex space-between">
+        <div class="desc flex align-center column space-between">
+            <div class="company flex space-around align-center">
                 <img class="owner-logo" :src="job.owner.logoUrl" />
-                <div class="job column flex align-center space-between">
+                <div class="details flex column align-center justify-center">
                     <h2>{{job.owner.name}}</h2>
-                    <h3>{{job.owner.rating}}☆</h3>
+                    <h4>{{job.owner.rating}}☆</h4>
+                </div>
+            </div>
+            <p v-if="job.owner.rating>4.5" class="top-comp">Top Company</p>
+            <div class="extra flex space-between align-center">
+                <div class="job flex space-between column align-center">
                     <h4>{{job.title}}</h4>
                     <p>{{job.loc.address}}</p>
                 </div>
-            </div>
-            <div class="extra flex space-between column align-center">
-                <h3 v-if="job.rating > 4.5">Top Company</h3>
                 <h4>83% match</h4>
             </div>
         </div>
@@ -29,9 +31,9 @@
 export default {
     props: { job: Object },
     data() {
-      return {
-        msg: ''
-      }
+        return {
+            msg: ''
+        }
     },
     methods: {
         goToDetails() {
