e036fb7c1b899bb9a252e54512885ec8c3f91c0c
Meital Lazarovich <lazarovich.meital@gmail.com>
2019-12-04 00:21:33 +0200

    merge bug fixing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,26 @@
 <template>
-  <header
-    class="flex space-between align-center"
-    :class="{'opened-menu': this.isMenuOpen, 'opened-user-menu': this.isUserMenuOpen}"
-  >
-    <div class="screen" @click="toggleMenu"></div>
-    <div class="logo-box flex align-center" @click="goHome">
-      <img src="../assets/logo.png" class="logo" />
-      <span>Jober</span>
-    </div>
-    <div class="flex align-center justify-center">
-      <nav @click="toggleMenu">
-        <router-link exact to="/">Home</router-link>
-        <router-link exact to="/job">Jobs</router-link>
-        <router-link exact to="/about">About</router-link>
-      </nav>
-      <div
-        class="user-menu flex column space-between align-center"
-        :class="{shown: isUserMenuOpen}"
-        @click="toggleUserMenu"
-      >
-        <h3 class="nav-btn semi flex-center" @click="goToProfile" v-if="isUserMenuOpen">Profile</h3>
-        <h3
-          class="nav-btn semi flex-center"
-          v-if="isUserMenuOpen && isCompany"
-          @click="goToCompPage"
-        >Back-Office</h3>
-        <h3 class="nav-btn semi flex-center" v-if="isUserMenuOpen" @click="logout">Logout</h3>
-      </div>
-      <img class="user-img" v-if="user" :src="userImg" @click="toggleUserMenu" />
-      <button v-else @click="goToLogin" class="login-btn">Login</button>
-      <button @click="toggleMenu" class="menu-btn">☰</button>
-    </div>
-  </header>
+      <header class="flex space-between align-center" :class="{'opened-menu': this.isMenuOpen, 'opened-user-menu': this.isUserMenuOpen}">
+        <div class="screen" @click="toggleMenu"></div>
+        <div class="logo-box flex align-center" @click="goHome">
+            <img src="../assets/logo.png" class="logo" />
+            <span>Jober</span>
+        </div>
+        <div class="flex align-center justify-center">
+            <nav @click="toggleMenu">
+                <router-link exact to="/">Home</router-link>
+                <router-link exact to="/job">Jobs</router-link>
+                <router-link exact to="/about">About</router-link>
+            </nav>
+            <div v-if="user" class="user-menu flex column space-between align-center" :class="{shown: isUserMenuOpen}" @click="toggleUserMenu">
+                <h3 class="nav-btn semi flex-center" @click="goToProfile" v-if="isUserMenuOpen">Profile</h3>
+                <h3 class="nav-btn semi flex-center" v-if="isUserMenuOpen && isCompany" @click="goToCompPage">Back-Office</h3>
+                <h3 class="nav-btn semi flex-center" v-if="isUserMenuOpen" @click="logout">Logout</h3>
+            </div>
+            <img class="user-img" v-if="user" :src="userImg" @click="toggleUserMenu" />
+            <button v-else @click="goToLogin" class="login-btn">Login</button>
+            <button @click="toggleMenu" class="menu-btn">☰</button>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -46,14 +35,6 @@ export default {
       isUserMenuOpen: false
     };
   },
-  methods: {
-    goHome() {
-      if (this.$router.currentRoute.fullPath === "/") return;
-      this.$router.push("/");
-    },
-    logout() {
-      this.$store.dispatch({ type: "logout", user: this.user });
-    },
     methods: {
         goHome() {
             this.$router.push('/');
@@ -82,20 +63,6 @@ export default {
         goToCompPage() {
             this.$router.push(`/comp`)
         }
-    },
-    toggleUserMenu() {
-      this.isMenuOpen = false;
-      this.isUserMenuOpen = !this.isUserMenuOpen;
-    },
-    goToLogin() {
-      this.$router.push("/login");
-    },
-    goToProfile() {
-      this.$router.push(`/user/${this.user._id}`);
-    },
-    goToCompPage() {
-      this.$router.push(`/comp`);
-    }
   },
   computed: {
     userImg() {
