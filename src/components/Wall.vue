5ef5090df48ca6b4a01b62c17d4ed7db37b01433
kostagon <49278316+kostagon@users.noreply.github.com>
2019-12-03 15:31:40 +0200

    username pick feature
    if user is not logged in,
    he will automatically post as "anonymous"
    if he is logged in, modal will pop and ask him in under which name he wants to post the comment.

--- src/components/Wall.vue
+++ src/components/Wall.vue
@@ -9,10 +9,32 @@
         :key="idx"
       ></Post>
     </div>
-
+    <div
+      v-if="isModalActive"
+      class="name-modal flex justify-center align-center"
+    >
+      <div>
+        Post as
+        <select v-model="nameOnPost">
+          <option>{{userName}}</option>
+          <option>Anonymous</option>
+        </select>&nbsp;
+        <button @click="addPost">Post</button>
+      </div>
+      <a
+        href="#"
+        class="cancel-btn"
+        @click.prevent="clearPost"
+      >x</a>
+    </div>
     <div class="add-post flex">
-      <textarea type="text" v-model="postToAdd.txt" />
-      <button @click="addPost">Post</button>
+      <textarea
+        type="text"
+        v-model="postToAdd.txt"
+      />
+      <div class="flex align-center">
+        <button v-if="!isModalActive" @click="onAddPost">Post</button>
+      </div>
     </div>
   </section>
 </template>
@@ -25,13 +47,22 @@ export default {
   },
   data() {
     return {
+      nameOnPost: null,
+      userName: null,
       postToAdd: null,
-      copyJob: null
+      copyJob: null,
+      isModalActive: false
     };
   },
   methods: {
+    onAddPost() {
+      if (this.postToAdd.txt.length <= 2) return;
+      else if (!this.userName) return this.addPost();
+      this.isModalActive = true;
+    },
     addPost() {
       if (this.postToAdd.txt.length <= 2) return;
+      this.postToAdd.from = this.nameOnPost;
       this.copyJob.posts.unshift(this.postToAdd);
       this.updateJob();
       this.clearPost();
@@ -45,16 +76,21 @@ export default {
     },
     clearPost() {
       this.postToAdd = {
-        from: "Anonymous",
         txt: "",
         createdAt: Date.now(),
         likes: 0
       };
+      this.isModalActive = false;
     }
   },
   created() {
     this.clearPost();
     this.copyJob = JSON.parse(JSON.stringify(this.job));
+    const user = this.$store.getters.loggedinUser;
+    if(user) {
+      this.nameOnPost = user.username;
+      this.userName = user.username;
+    } else this.nameOnPost = 'Anonymous';
   },
   props: ["job"]
 };
