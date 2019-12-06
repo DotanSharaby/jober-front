f7b9f582e6f25162fe2fc64ba851d4d16c6965a5
kostagon <kostagon@users.noreply.github.com>
2019-12-06 19:00:52 +0200

    merged

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,49 +1,56 @@
 <template>
-    <section class="login">
-        <h1 class="semi">Login</h1>
+  <section class="login">
+    <h1 class="semi">Login</h1>
 
-        <form @submit.prevent="doLogin" class="flex column">
-            <label>Email:</label>
-            <input type="email" v-model="loginCred.email" placeholder="Email" />
+    <form @submit.prevent="doLogin" class="flex column">
+      <label>Email:</label>
+      <input type="email" ref="input" v-model="loginCred.email" placeholder="Email" />
 
-            <label>Password:</label>
-            <input type="password" v-model="loginCred.pass" placeholder="Password" />
+      <label>Password:</label>
+      <input type="password" v-model="loginCred.pass" placeholder="Password" />
 
-            <button class="save-btn">Login</button>
-        </form>
-        <h3 class="error">{{ msg }}</h3>
-        <h3 class="signup-msg text-center">
-            Don't have account yet?
-            <router-link to="/signup" class="semi profile-link">Sign up</router-link>
-        </h3>
-    </section>
+      <button class="save-btn">Login</button>
+    </form>
+    <h3 class="error">{{ msg }}</h3>
+    <h3 class="signup-msg text-center">
+      Don't have account yet?
+      <router-link to="/signup" class="semi profile-link">Sign up</router-link>
+    </h3>
+  </section>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            loginCred: {
-                email: '',
-                pass: ''
-            },
-            msg: ''
-        };
-    },
-    methods: {
-        async doLogin() {
-            const cred = this.loginCred;
-            if (!cred.email || !cred.pass) return (this.msg = "Please fill both fields");
-            const user = await this.$store.dispatch({ type: "login", userCred: cred });
-            if (!user) return this.msg = "Wrong email or password";
-            this.msg = "";
-            this.$router.push("/");
-            this.loginCred = {};
-        }
-    },
-    created() {
-        const user = this.$store.getters.loggedinUser;
-        if (user) this.$router.push('/')
+  data() {
+    return {
+      loginCred: {
+        email: "",
+        pass: ""
+      },
+      msg: ""
+    };
+  },
+  methods: {
+    async doLogin() {
+      const cred = this.loginCred;
+      if (!cred.email || !cred.pass)
+        return (this.msg = "Please fill both fields");
+      const user = await this.$store.dispatch({
+        type: "login",
+        userCred: cred
+      });
+      if (!user) return (this.msg = "Wrong email or password");
+      this.msg = "";
+      this.$router.push("/");
+      this.loginCred = {};
     }
+  },
+  created() {
+    const user = this.$store.getters.loggedinUser;
+    if (user) this.$router.push("/");
+  },
+  mounted() {
+    this.$refs.input.focus();
+  }
 };
 </script>
