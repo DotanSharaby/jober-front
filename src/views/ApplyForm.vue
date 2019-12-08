8153a006e857a1adede9aa695ef25aac25fa7150
DotanSharaby <dotan.sharaby@gmail.com>
2019-12-08 17:02:55 +0200

    heroku

--- src/views/ApplyForm.vue
+++ src/views/ApplyForm.vue
@@ -1,70 +1,78 @@
 <template>
-    <section
-        class="apply-form-wrapper flex space-between align-center column container"
-        v-if="currJob"
-    >
-        <span @click="goBack" class="back-btn">⬅</span>
-        <div class="header">
-            <div>
-                <h2 class="bold">{{currJob.title}} - {{currJob.owner.username}}</h2>
-            </div>
-        </div>
-        <div class="main flex space-between align-center">
-            <section class="apply-list flex justify-center column">
-                <h3
-                    class="semi"
-                >Please record a short video of yourself, and refer to the following:</h3>
-                <ul class="clean-list">
-                    <li v-for="quest in currJob.quests" :quest="quest" :key="quest">- {{ quest }}</li>
-                </ul>
-            </section>
-            <VideoCapture class="video" :uploadUrl="serverUrl" v-model="application.videoUrl"></VideoCapture>
-            <div class="pm flex-center">
-                <textarea v-model="application.pm" placeholder="Add a personal message (optional)"></textarea>
-            </div>
-        </div>
-        <button class="semi submit" @click.once="submit">Submit</button>
-    </section>
+  <section
+    class="apply-form-wrapper flex space-between align-center column container"
+    v-if="currJob"
+  >
+    <span @click="goBack" class="back-btn">⬅</span>
+    <div class="header">
+      <div>
+        <h2 class="bold">{{currJob.title}} - {{currJob.owner.username}}</h2>
+      </div>
+    </div>
+    <div class="main flex space-between align-center">
+      <section class="apply-list flex justify-center column">
+        <h3 class="semi">Please record a short video of yourself, and refer to the following:</h3>
+        <ul class="clean-list">
+          <li v-for="quest in currJob.quests" :quest="quest" :key="quest">- {{ quest }}</li>
+        </ul>
+      </section>
+      <scale-loader v-if="isLoading" :color="'#8bdade'"></scale-loader>
+      <VideoCapture class="video" ref="video" :uploadUrl="serverUrl" v-model="application.videoUrl"></VideoCapture>
+      <div class="pm flex-center">
+        <textarea v-model="application.pm" placeholder="Add a personal message (optional)"></textarea>
+      </div>
+    </div>
+    <button class="semi submit" @click.once="submit">Submit</button>
+  </section>
 </template>
 
 <script>
 import { VideoCapture } from "vue-media-recorder";
+import ScaleLoader from "vue-spinner/src/ScaleLoader.vue";
 
 export default {
-    data() {
-        return {
-            currJob: null,
-            user: {},
-            application: { pm: "", videoUrl: null },
-            serverUrl: "https://mister-recorder.herokuapp.com/uploads/"
-        };
+  data() {
+    return {
+      currJob: null,
+      user: {},
+      application: { pm: "", videoUrl: null },
+      serverUrl: "https://mister-recorder.herokuapp.com/uploads/",
+      isLoading: false
+    };
+  },
+  methods: {
+    goBack() {
+      return this.$router.go(-1);
     },
-    methods: {
-        goBack() {
-            return this.$router.go(-1);
-        },
-        submit() {
-            var userInfo = this.$store.getters.userInfo;
-            this.application = { ...this.application, ...userInfo };
-            this.user.appliedJobsIds.push(this.currJob._id);
-            if (this.currJob.applicants) {
-                this.currJob.applicants.push(this.application);
-            } else {
-                this.currJob.applicants = [this.application];
-            }
-            const app = { job: this.currJob, user: this.user };
-            this.$store.dispatch({ type: "applyForm", app });
-            return this.$router.push("/");
-        }
-    },
-    created() {
-        this.currJob = this.$store.getters.currJob
-        this.currJob = JSON.parse(JSON.stringify(this.currJob))
-        this.user = this.$store.getters.loggedinUser
-        this.user = JSON.parse(JSON.stringify(this.user))
-    },
-    components: {
-        VideoCapture
+    submit() {
+      var userInfo = this.$store.getters.userInfo;
+      this.application = { ...this.application, ...userInfo };
+      this.user.appliedJobsIds.push(this.currJob._id);
+      if (this.currJob.applicants) {
+        this.currJob.applicants.push(this.application);
+      } else {
+        this.currJob.applicants = [this.application];
+      }
+      const app = { job: this.currJob, user: this.user };
+      this.$store.dispatch({ type: "applyForm", app });
+      return this.$router.push("/");
     }
+  },
+  created() {
+    this.currJob = this.$store.getters.currJob;
+    this.currJob = JSON.parse(JSON.stringify(this.currJob));
+    this.user = this.$store.getters.loggedinUser;
+    this.user = JSON.parse(JSON.stringify(this.user));
+  },
+  mounted() {
+    this.$watch("$refs.video.isUploading", new_value => {
+      if (new_value) this.isLoading = true;
+      else this.isLoading = false;
+    });
+  },
+  components: {
+    VideoCapture,
+    ScaleLoader
+  }
 };
 </script>
